3f83e86293704368d7155ca51b9c884d7f3abc0d
Salman Khan <salmantechhub336@gmail.com>
2026-04-08 23:59:40 +0500

    setup api key in ios and android files

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import Flutter
 import UIKit
+import GoogleMaps
 
 @main
 @objc class AppDelegate: FlutterAppDelegate, FlutterImplicitEngineDelegate {
@@ -7,6 +8,7 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+     GMSServices.provideAPIKey("AIzaSyC2dbOL2kbrs3zG7pd_n4DbMRfFomZ9iHg")
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 
